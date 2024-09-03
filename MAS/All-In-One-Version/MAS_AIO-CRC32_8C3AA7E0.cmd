255f6d44bdeaf01852ecaca103f854980148d0d2
jkinformaticashop <contato@jkinformatica.shop>
2024-09-02 21:46:05 -0300

    Update MAS_AIO-CRC32_8C3AA7E0.cmd

--- MAS/All-In-One-Version/MAS_AIO-CRC32_8C3AA7E0.cmd
+++ MAS/All-In-One-Version/MAS_AIO-CRC32_8C3AA7E0.cmd
@@ -271,22 +271,20 @@ echo:
 echo:
 echo:       ______________________________________________________________
 echo:
-echo:                 Activation Methods:
+echo:             ATIVE O PACOTE OFFICE:
 echo:
-echo:             [1] HWID        ^|  Windows           ^|   Permanent
-echo:             [2] Ohook       ^|  Office            ^|   Permanent
-echo:             [3] KMS38       ^|  Windows           ^|   Year 2038
-echo:             [4] Online KMS  ^|  Windows / Office  ^|    180 Days
+echo:             
+echo:             [1] Ativar      ^|  Office      ^|   Permanente
+echo:                        
 echo:             __________________________________________________      
-echo:
-echo:             [5] Activation Status
-echo:             [6] Troubleshoot
-echo:             [7] Extras
-echo:             [8] Help
+echo:                   
+echo:             [2] Install Office
+echo:             [3] Suporte
 echo:             [0] Exit
 echo:       ______________________________________________________________
 echo:
-call :_color2 %_White% "          " %_Green% "Enter a menu option in the Keyboard [1,2,3,4,5,6,7,8,0] :"
+call :_color2 %_White% "          " %_Green% "Enter a menu option in the Keyboard [1,2,3,0] :"
+
 choice /C:123456780 /N
 set _erl=%errorlevel%
 
